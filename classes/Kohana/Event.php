c26df75d7e3fc6320887b1ad0b1ac5836bd72e6d
Karol Wnuk <karol.wnuk@redexperts.net>
2013-07-05 17:54:36 +0200

    Tabs to spaces

--- classes/Kohana/Event.php
+++ classes/Kohana/Event.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 /**
- * This class implements an event/trigger calling system.  functions or class/methods can be assigned to a event 'key' and will be run when that point in the code is called.  
+ * This class implements an event/trigger calling system.  functions or class/methods can be assigned to a event 'key' and will be run when that point in the code is called.
  * Variables are passed by reference so that external functions can modify internal variables directly
- * 
+ *
  * Event::register('user_login','my_custom_function');
  * Event::trigger('user_login',$User);
  * function my_custom_function($User) {
@@ -12,14 +12,14 @@
  * }
  */
 
-class Kohana_Event 
+class Kohana_Event
 {
     public $events = array();
     protected static $__instance = NULL;
 
     /**
      * factory
-     * 
+     *
      * @access public
      * @static
      *
@@ -32,7 +32,7 @@ class Kohana_Event
 
     /**
      * instance
-     * 
+     *
      * @access public
      * @static
      *
@@ -49,7 +49,7 @@ class Kohana_Event
 
     /**
      * events
-     * 
+     *
      * @param mixed $key = NULL send a key to retrieve all callbacks on a specific key.  If no key is sent, an array of all callbacks for all keys is returned
      * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used
      *
@@ -70,7 +70,7 @@ class Kohana_Event
         }
         else if (isset($Event->events[$key]))
         {
-            return $Event->events[$key];   
+            return $Event->events[$key];
         }
         else
         {
@@ -83,7 +83,7 @@ class Kohana_Event
      * registers a callback to a event key
      * @param string $key event type identifier
      * @param string|array|closure $callback if this is a string, it must exist as a function.  if it is an array, then it must be callable (passed as [$object,'method'] or ['class','method']). it can also be sent as a closure
-     * @param array|NULL $args variable(s) passed by reference as args to the callback.  these args override any $args sent to callback by the 'trigger' method    
+     * @param array|NULL $args variable(s) passed by reference as args to the callback.  these args override any $args sent to callback by the 'trigger' method
      * @param array $options array containing any additional event options (here for future features). current valid options are max-runs
      * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used
      * @return string $id and identifier for this specific event registration, can be used to un-register
@@ -99,12 +99,12 @@ class Kohana_Event
         $Event->events[$key][$id] = array('callback'=> $callback, 'args'=> &$args, 'options'=> $options);
         return $id;
     }
-    
+
     /**
      * removes a callback based on an $id returned from event::register
      * @param string $key event type identifier
      * @param string $id event identifier
-     * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used     
+     * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used
      * @return boolean return TRUE if they $key.$id was set and removed, return FALSE if $key.$id was not set
      */
     public static function remove($key, $id, Event $Event = NULL)
@@ -112,7 +112,7 @@ class Kohana_Event
         if ($Event === NULL)
         {
             $Event = self::instance();
-        }        
+        }
         if (isset($Event->events[$key][$id]))
         {
             unset($Event->events[$key][$id]);
@@ -128,41 +128,41 @@ class Kohana_Event
      * triggers all callbacks for a given event key with $args passed by reference
      * @param $key (string) event identifier
      * @param $callback (string|function|(class:method)|closure) if this is a string, it must exist as a function.  if it is an array, then it must be callable (passed as [$object,'method'] or ['class','method'])
-     * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used     
-     */ 
+     * @param Event = NULL send along an event instance to apply this event to.  If no instance is set, the default instance is used
+     */
     public static function trigger($key,&$args = NULL, Event $Event = NULL)
     {
         if ($Event === NULL)
         {
             $Event = self::instance();
-        }           
+        }
         //return null if there are no callbacks assigned to this key
         if (!isset($Event->events[$key]))
         {
             return NULL;
         }
-        
+
         //loop through each callback assigned to this key
         foreach ($Event->events[$key] as $i => $event)
         {
 
             // If no arguments were sent to "trigger" and there were arguments sent to "register"
-            if ($event['args'] !== NULL && $args === NULL) 
+            if ($event['args'] !== NULL && $args === NULL)
             {
-                $args = &$event['args']; 
+                $args = &$event['args'];
             }
-            
-            try 
+
+            try
             {
                 $ref_args = array(&$args);
                 $call = Args::call($event['callback'],$ref_args);
-            } 
-            catch (Exception $e) 
+            }
+            catch (Exception $e)
             {
-            	throw $e;
+                throw $e;
                 //throw new Kohana_Exception('event call for key "'.$key.'" caused exception: '.$e->getMessage());
-            }           
+            }
         }
     }
-    
+
 }
